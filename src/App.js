6b05a4871b2c0d964a5c9bbfce0ec22ab8210f7a
Carl Gao <carlgao117@gmail.com>
2020-08-02 14:21:19 -0700

    Change <App> to function component

--- src/App.js
+++ src/App.js
@@ -5,28 +5,17 @@ import DATA from "./data.js";
 // Components
 import SliderInput from "./SliderInput.js";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { age: 1.5, weight: 45.0, medSet: new Set() };
-    console.log(DATA);
-    console.log(this.state);
-  }
-
-  render() {
-    const handleAgeChange = (event, newValue) => {
-      console.log(event, newValue);
-    };
-    return (
-      <div className="App">
-        <header className="App-header">
-          Pediatric Anesthesiology Helper
-          <SliderInput name="Age" onChange={handleAgeChange} />
-          Show dosages for:
-        </header>
-      </div>
-    );
-  }
+export default function App() {
+  const handleAgeChange = (event, newValue) => {
+    console.log(event, newValue);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        Pediatric Anesthesiology Helper
+        <SliderInput name="Age" onChange={handleAgeChange} />
+        Show dosages for:
+      </header>
+    </div>
+  );
 }
-
-export default App;
