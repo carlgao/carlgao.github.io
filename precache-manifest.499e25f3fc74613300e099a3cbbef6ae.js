9049221b38c9aee7ca572027f5cd421a405f388d
Carl Gao <carlgao117@gmail.com>
2020-08-08 17:07:06 -0700

    Updates

---
+++ precache-manifest.499e25f3fc74613300e099a3cbbef6ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4387c137579477ced4c0e312360ffd88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37a389ae601e8837f5b1",
+    "url": "/static/css/main.b566c2e6.chunk.css"
+  },
+  {
+    "revision": "9602d1d95c2b420658dd",
+    "url": "/static/js/2.9af4bd7f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.9af4bd7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37a389ae601e8837f5b1",
+    "url": "/static/js/main.ec8517c9.chunk.js"
+  },
+  {
+    "revision": "b71d377d7b8c4516202a",
+    "url": "/static/js/runtime-main.cd036396.js"
+  }
+]);
